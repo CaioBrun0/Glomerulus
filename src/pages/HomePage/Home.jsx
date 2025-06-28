3439ef90d8b6c68efcbcc161466ee040d67fa20a
CaioBrun0 <kmbmatos2@gmail.com>
2025-06-28 04:12:27 -0300

    Criando cards de informações que ficam no topo da página Home

--- src/pages/HomePage/Home.jsx
+++ src/pages/HomePage/Home.jsx
@@ -1,13 +1,34 @@
 import Menu from "../../components/Menu/Menu"
 import CardAmbiente from "../../components/cardAmbiente/cardAmbiente"
+import Imagem1 from "../../assets/img-inovacao-medica.jpg"
+import Imagem2 from "../../assets/card1.png"
+import Imagem3 from "../../assets/card2.png"
 import "./Home.css"
 
 
 function Home() {
+    const imagensAvaliadas = 0;
+    const ambiantesDisponiveis = 3;
     return(
         <>        
-        <CardAmbiente type="Crescente" amount={512} />
-        <Menu/>
+        <div className="main-content">
+            <Menu/>
+            <div className="topo">
+                <img src={Imagem1} alt="inovacaoMedica" className="img-inovacao"/>
+
+                <div alt="avaliadas" className="cardTopo">
+                    <img src={Imagem2} alt="card1" />
+                    <p id="titulo"><span className="numero">{imagensAvaliadas}</span> Imagens Avaliadas</p>
+                    <p>Para cada ambiente concluido, você contribui para o avanço da ciência</p>
+                </div>
+
+                <div alt="ambientes" className="cardTopo">
+                    <img src={Imagem3} alt="card2" />
+                    <p id="titulo"><span className="numero">{ambiantesDisponiveis}</span> Ambientes disponíveis</p>
+                    <p>Não deixe para amanhã o que você pode fazer hoje</p>
+                </div>
+            </div>
+        </div>
         </>
     )
 }
